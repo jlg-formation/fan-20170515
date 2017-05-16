91a3a913e5ce467a1410535aec8fe76c50885d99
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-16 16:55:49 +0200

    Add:Exo 3

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var app = angular.module('main', ['ors-star']);
+    var app = angular.module('main', ['ors-star', 'ors-route']);
 
     app.directive('orsHeader', function () {
         return {
---
+++ app/ors-route/ors-route.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    var app = angular.module('ors-route', ['ui.router']);
+
+    app.config(function ($stateProvider) {
+        'ngInject';
+        $stateProvider.state({
+            name: 'home',
+            url: '/',
+            templateUrl: './ors-route/tmpl/home.html'
+        });
+        $stateProvider.state({
+            name: 'products',
+            url: '/produits',
+            templateUrl: './ors-route/tmpl/products.html'
+        });
+        $stateProvider.state({
+            name: 'services',
+            url: '/services',
+            templateUrl: './ors-route/tmpl/services.html'
+        });
+        $stateProvider.state({
+            name: 'contact',
+            url: '/contact',
+            templateUrl: './ors-route/tmpl/contact.html'
+        });
+    });
+
+})();
