2ecee9f54207094c9d608c90359c9d6a94345ea9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-16 12:11:42 +0200

    done

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -13,20 +13,31 @@
             scope: {
                 n: '=?note'
             },
-            controller: function OrsHeaderCtrl($scope, $attrs) {
+            controller: function OrsHeaderCtrl($scope, $element, $attrs, $compile) {
                 'ngInject';
                 console.log('OrsStarCtrl', arguments);
-                $scope.getImage = function (i) {
-                    var n2 = $scope.n || 3;
-                    if (i > n2) {
-                        return 'ors-star/img/white_star.png';
-                    }
-                    return 'ors-star/img/yellow_star.png';
-                };
+
                 $scope.update = function (i) {
                     console.log('update');
                     $scope.n = i;
-                }
+                };
+
+                $scope.$watch('n', function () {
+                    var html = '';
+                    var n2 = $scope.n || 3;
+                    n2 = (n2 > 5) ? 5 : n2;
+                    n2 = (n2 < 0) ? 0 : n2;
+                    for (var i = 0; i < n2; i++) {
+                        html += '<img ng-click="update(' + (i + 1) + ')" src="ors-star/img/yellow_star.png"/>';
+                    }
+                    for (i = n2; i < 5; i++) {
+                        html += '<img ng-click="update(' + (i + 1) + ')" src="ors-star/img/white_star.png"/>';
+                    }
+                    $element.html(html);
+                    // $compile pour compiler et puis $element.contents() pour recuperer le DOM
+                    $compile($element.contents())($scope);
+                });
+
             }
         };
     });
