cf1aab3053114d5cbda4cfe5b12eb91fcc20f409
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-16 11:00:56 +0200

    aa

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,10 +6,22 @@
     app.directive('orsStar', function () {
         return {
             restrict: 'E',
-            template: '<img ng-repeat="i in [1,2,3,4,5]" src="ors-star/img/yellow_star.png"/>',
-            controller: function OrsHeaderCtrl() {
+            template: function () {
+                return '<img ng-repeat="i in [1,2,3,4,5]" ng-src="{{getImage(i)}}"/>';
+            },
+            controller: function OrsHeaderCtrl($scope) {
                 'ngInject';
                 console.log('OrsStarCtrl', arguments);
+                var note = 3;
+                $scope.getImage = function (i) {
+                    if (i > note) {
+                        return 'ors-star/img/white_star.png';
+                    }
+
+                    return 'ors-star/img/yellow_star.png';
+
+
+                }
             },
         };
     });
